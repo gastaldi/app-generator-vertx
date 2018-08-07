77c4f6e82f7b55af32481da950cb4411ad8d1e7a
George Gastaldi <gegastaldi@gmail.com>
2018-08-07 17:00:43 -0300

    It Works

--- src/main/java/io/fabric8/launcher/vertx/ScriptHandler.java
+++ src/main/java/io/fabric8/launcher/vertx/ScriptHandler.java
@@ -3,18 +3,13 @@ package io.fabric8.launcher.vertx;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-
 import io.fabric8.launcher.util.Zip;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.RoutingContext;
-import org.jboss.forge.addon.resource.DirectoryResource;
-import org.jboss.forge.addon.resource.ResourceFactory;
-import org.jboss.forge.addon.script.ScriptContextBuilder;
-import org.jboss.forge.addon.script.impl.ForgeScriptEngineFactory;
+import org.jboss.forge.addon.script.ScriptOperations;
+import org.jboss.forge.addon.ui.result.Result;
 
 /**
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
@@ -24,22 +19,15 @@ public class ScriptHandler implements Handler<RoutingContext> {
     public void handle(RoutingContext routingContext) {
         String body = routingContext.getBodyAsString();
         try {
-            ResourceFactory resourceFactory = FurnaceVerticle.furnace.getAddonRegistry().getServices(ResourceFactory.class).get();
-            ForgeScriptEngineFactory scriptEngineFactory = FurnaceVerticle.furnace.getAddonRegistry().getServices(ForgeScriptEngineFactory.class).get();
-
-            ScriptEngine engine = scriptEngineFactory.getScriptEngine();
+            ScriptOperations scriptOperations = FurnaceVerticle.furnace.getAddonRegistry().getServices(ScriptOperations.class).get();
             Path tempDirectory = Files.createTempDirectory("tmp");
-            DirectoryResource resource = resourceFactory.create(DirectoryResource.class, tempDirectory.toFile());
-            ScriptContext context = ScriptContextBuilder.create()
-                    .currentResource(resource)
-                    .build();
-            engine.eval(body, context);
+            Result result = scriptOperations.executeScript(tempDirectory.toFile(), body, null, null);
             byte[] contents = Zip.zip("application", tempDirectory);
             Path tempFile = Files.createTempFile("project", ".zip");
             Files.write(tempFile, contents);
             HttpServerResponse response = routingContext.response();
             response.headers().set(HttpHeaders.CONTENT_TYPE, "application/zip");
-            response.sendFile(tempFile.toString()).end();
+            response.sendFile(tempFile.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
